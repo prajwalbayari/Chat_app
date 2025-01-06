0258452a4d36dcb02b7bc7bce4b4051c0e2277e4
prajwalbayari <prajwalbayari4@gmail.com>
2025-01-07 00:22:08 +0530

    Added Login and logout

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import appRoutes from "./routes/auth.routes.js"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser"
 
 dotenv.config()
 const PORT=process.env.PORT;
@@ -9,6 +10,7 @@ const PORT=process.env.PORT;
 const app=express()
 
 app.use(express.json()) //Helps to extract data from request body
+app.use(cookieParser()) //Verify cookies
 
 app.use("/api/auth",appRoutes)
 
