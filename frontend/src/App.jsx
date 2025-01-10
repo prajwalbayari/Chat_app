76b5db8072422d784e919ad689a9e975d7e2877d
prajwalbayari <prajwalbayari4@gmail.com>
2025-01-10 23:30:30 +0530

    Added settings page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,9 +9,11 @@ import ProfilePage from "./pages/ProfilePage";
 import { userAuthStore } from "./store/userAuthStore";
 import { Loader, LogIn } from "lucide-react";
 import { Toaster } from "react-hot-toast";
+import { useThemeStore } from "./store/useThemeStore";
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = userAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -27,7 +29,7 @@ const App = () => {
     );
 
   return (
-    <div>
+    <div data-theme={theme} >
       <Navbar />
       <Routes>
         <Route
