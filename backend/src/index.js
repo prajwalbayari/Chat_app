a5c27b1634725563418d69e94eb7ad3948d952fc
prajwalbayari <prajwalbayari4@gmail.com>
2025-02-11 20:48:09 +0530

    Minor changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,10 +28,10 @@ app.use("/api/auth", appRoutes);
 app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
